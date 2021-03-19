d4d930d6e7a51c97eea1d20c84adf320d7722c05
Valentine SCHALCKENS <v.schalckens@gmail.com>
2021-03-19 11:02:46 +0100

    exo 1 terminé

--- exo1.php
+++ exo1.php
@@ -6,3 +6,16 @@
  * and open the template in the editor.
  */
 
+echo 'Écrire un programme PHP qui affiche tous les nombres impairs   entre 0 et 10000, par ordre croissant : "1 3 5 7 ... 9995 9997 9999" </br>';
+
+
+for($i = 0; $i < 10000; $i++)
+{
+    if($i % 2 !== 0) 
+    {
+        print($i);
+        echo '</br>';
+    }
+}
+
+?>
