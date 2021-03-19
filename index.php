b7f085e29ab0a4a6ccb2f91d29d09c17ac027b9a
Valentine SCHALCKENS <v.schalckens@gmail.com>
2021-03-19 10:53:29 +0100

    Ajout de la page index

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<html><body>
+        <?php
+        /*
+         * To change this license header, choose License Headers in Project Properties.
+         * To change this template file, choose Tools | Templates
+         * and open the template in the editor.
+         */
+
+        echo 'Hello, Voici un entrainement en PHP !';
+        ?>
+        <div>
+            <ul>
+                <li>
+                    <a href="exo1.php">Exercice 1</a>
+                </li>
+            </ul>
+        </div>
+    </body>
+</html>
